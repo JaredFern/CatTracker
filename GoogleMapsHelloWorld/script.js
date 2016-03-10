cb81d25c718f0908836927b61065f59ddb2cfa7a
isaacrlee <isaacrlee@gmail.com>
2016-03-10 17:45:41 -0600

    Updated Map (Index)

--- GoogleMapsHelloWorld/script.js
+++ GoogleMapsHelloWorld/script.js
@@ -5,15 +5,45 @@ function initMap() {
         zoom: 5
     });
 
-    locations = JSON.parse('places.json');
-
-    for (var incident in locations) {
-      var marker = new google.maps.Marker({
-      	position: locations[incident].center,
-      	map:map
-      })
+    for (var i = 0; i < 20; i++) {
+        var myLatLng = {lat: places[i].lat, lng: places[i].lng};
+        var contentString =
+            '<div id="content">' + 'city: ' + places[i].city + '<br>'
+            + 'company: ' + places[i].company + '<br>'
+            + 'popularity: ' + places[i].popularity
+            +  '</div>';
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: places[i].city,
+            info: contentString
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent( this.info);
+            infowindow.open(map, this);
+        });
     }
 
-    //map.data.loadGeoJson('places.json');
 
-    }
+}
+
+//function initMap() {
+//    console.log("Hello");
+//
+//    var map = new google.maps.Map(document.getElementById('map'), {
+//        zoom: 5,
+//        center: {lat: 37.2756895, lng: -104.6556336}
+//    });
+//
+//    var myLatLng = {lat: places[0].lat, lng: places[0].lng};
+//
+//    var marker = new google.maps.Marker({
+//        position: myLatLng,
+//        map: map,
+//        title: 'Hello World!'
+//    });
+//
+//}
